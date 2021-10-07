9d67ff4fef385a115deefe4c3151cb55dc78a47b
Sierra Nicholes <s.k.nicholes@gmail.com>
2021-10-07 18:02:46 -0400

    promises?

--- bot.js
+++ bot.js
@@ -9,37 +9,39 @@ let client = new AWS.SecretsManager({
     region: region
 });
 
-getMsg();
+doMsg();
 
-async function getMsg() {
-    await client.getSecretValue({SecretId: secretName}, function(err, data) {
-        if (err) {
-            if (err.code === 'DecryptionFailureException')
-                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                throw err;
-            else if (err.code === 'InternalServiceErrorException')
-                // An error occurred on the server side.
-                throw err;
-            else if (err.code === 'InvalidParameterException')
-                // You provided an invalid value for a parameter.
-                throw err;
-            else if (err.code === 'InvalidRequestException')
-                // You provided a parameter value that is not valid for the current state of the resource.
-                throw err;
-            else if (err.code === 'ResourceNotFoundException')
-                // We can't find the resource that you asked for.
-                throw err;
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        if (err.code === 'DecryptionFailureException')
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            throw err;
+        else if (err.code === 'InternalServiceErrorException')
+            // An error occurred on the server side.
+            throw err;
+        else if (err.code === 'InvalidParameterException')
+            // You provided an invalid value for a parameter.
+            throw err;
+        else if (err.code === 'InvalidRequestException')
+            // You provided a parameter value that is not valid for the current state of the resource.
+            throw err;
+        else if (err.code === 'ResourceNotFoundException')
+            // We can't find the resource that you asked for.
+            throw err;
+    }
+    else {
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
         }
-        else {
-            // Depending on whether the secret is a string or binary, one of these fields will be populated.
-            if ('SecretString' in data) {
-                secret = data.SecretString;
-            } else {
-                let buff = new Buffer(data.SecretBinary, 'base64');
-                decodedBinarySecret = buff.toString('ascii');
-            }
-        }
-    });
+    }
+});
+
+async function doMsg() {
+    let secret = await client.getSecretValue({SecretId: secretName}).promise();
 
     let bot = new Discord.Client({
         token: secret.token,
